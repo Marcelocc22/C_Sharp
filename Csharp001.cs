cc3affccd01b546e6bc2462d7ec91976ae737e0f
Marcelocc22 <109487528+Marcelocc22@users.noreply.github.com>
2022-10-13 11:12:43 -0300

    news

--- Csharp001.cs
+++ Csharp001.cs
@@ -3,14 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Hello World
+namespace HelloWorld
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello World!")
-            
+            Console.Write("Hello World!");
+            Console.Write("Tecle Enter para sair." );
+            Console.ReadLine();
         } 
     }
 }
